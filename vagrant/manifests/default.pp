33dffb0d0edbb9c4bf050524419c3767eba1f7c9
Rob Hogan <rh389@cam.ac.uk>
2013-09-15 11:06:22 +0100

    Add vagrant and puppet

---
+++ vagrant/manifests/default.pp
@@ -0,0 +1,105 @@
+group { 'puppet': ensure => present }
+Exec { path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ] }
+File { owner => 0, group => 0, mode => 0644 }
+
+class {'apt':
+  always_apt_update => true,
+}
+
+Class['::apt::update'] -> Package <|
+    title != 'python-software-properties'
+and title != 'software-properties-common'
+|>
+
+    
+class { 'puphpet::dotfiles': }
+
+package { [
+    'build-essential',
+    'vim',
+    'curl',
+    'git-core'
+  ]:
+  ensure  => 'installed',
+}
+
+class { 'apache': }
+
+apache::dotconf { 'custom':
+  content => 'EnableSendfile Off',
+}
+
+apache::module { 'rewrite': }
+
+apache::vhost { 'default':
+  server_name   => 'cupsc.dev',
+  priority      => '',
+  serveraliases => [],
+  docroot       => '/var/www/cupsc/web',
+  port          => '80',
+  env_variables => [],
+}
+
+class { 'php':
+  service             => 'apache',
+  service_autorestart => false,
+  module_prefix       => '',
+}
+
+php::module { 'php5-cli': }
+php::module { 'php5-curl': }
+php::module { 'php5-intl': }
+php::module { 'php5-mcrypt': }
+php::module { 'php-apc': }
+
+class { 'php::devel':
+  require => Class['php'],
+}
+
+
+class { 'xdebug':
+  service => 'apache',
+}
+
+class { 'composer':
+  require => Package['php5', 'curl'],
+}
+
+puphpet::ini { 'xdebug':
+  value   => [
+    'xdebug.default_enable = 1',
+    'xdebug.remote_autostart = 0',
+    'xdebug.remote_connect_back = 1',
+    'xdebug.remote_enable = 1',
+    'xdebug.remote_handler = "dbgp"',
+    'xdebug.remote_port = 9000'
+  ],
+  ini     => '/etc/php5/conf.d/zzz_xdebug.ini',
+  notify  => Service['apache'],
+  require => Class['php'],
+}
+
+puphpet::ini { 'php':
+  value   => [
+    'date.timezone = "Europe/London"'
+  ],
+  ini     => '/etc/php5/conf.d/zzz_php.ini',
+  notify  => Service['apache'],
+  require => Class['php'],
+}
+
+puphpet::ini { 'custom':
+  value   => [
+    'display_errors = On',
+    'error_reporting = -1',
+    'allow_url_fopen = 0',
+    'apc.enabled = 1'
+  ],
+  ini     => '/etc/php5/conf.d/zzz_custom.ini',
+  notify  => Service['apache'],
+  require => Class['php'],
+}
+
+package { "acl":
+    ensure => "installed"
+}
